8581bf03c2113729fd394fff27ae6bd58fab3c66
FEH-SAMA <felipe.tomaz.rebeca.br@gmail.com>
2020-08-12 09:55:44 -0400

    Up Files '12/08 Zen'

--- app.sh
+++ app.sh
@@ -7,36 +7,89 @@
 echo 'Inicializando.'
 
 sleep 1
-./database_checker.sh | zenity --progress --title="$TITLE" --text="Verificando os bancos de dados, Aguarde ..." --auto-close --no-cancel --pulsate --time-remaining
+./database_checker.sh | zenity --progress --title="$TITLE" --text="Verificando bancos de dados, Aguarde ..." --auto-close --no-cancel --pulsate --time-remaining
 
 sleep 1
 clear
 sleep 3
 
-echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
-echo '░░░█████╗░███╗░░██╗██╗███╗░░░███╗███████╗░██████╗░░░░░░░░░░░░░░░'; sleep 1
-echo '░░██╔══██╗████╗░██║██║████╗░████║██╔════╝██╔════╝░░░░░░░░░░░░░░░'; sleep 1
-echo '░░███████║██╔██╗██║██║██╔████╔██║█████╗░░╚█████╗░░░░░░░░░░░░░░░░'; sleep 1
-echo '░░██╔══██║██║╚████║██║██║╚██╔╝██║██╔══╝░░░╚═══██╗░░░░░░░░░░░░░░░'; sleep 1
-echo '░░██║░░██║██║░╚███║██║██║░╚═╝░██║███████╗██████╔╝░░░░░░░░░░░░░░░'; sleep 1
-echo '░░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░░░░╚═╝╚══════╝╚═════╝░░░░░░░░░░░░░░░░'; sleep 1
-echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
-echo '░░░░░░░░░░██╗░░██╗░█████╗░██████╗░██╗███████╗░█████╗░███╗░░██╗░░'; sleep 1
-echo '░░░░░░░░░░██║░░██║██╔══██╗██╔══██╗██║╚════██║██╔══██╗████╗░██║░░'; sleep 1
-echo '░░░░░░░░░░███████║██║░░██║██████╔╝██║░░███╔═╝██║░░██║██╔██╗██║░░'; sleep 1
-echo '░░░░░░░░░░██╔══██║██║░░██║██╔══██╗██║██╔══╝░░██║░░██║██║╚████║░░'; sleep 1
-echo '░░░░░░░░░░██║░░██║╚█████╔╝██║░░██║██║███████╗╚█████╔╝██║░╚███║░░'; sleep 1
-echo '░░░░░░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚══════╝░╚════╝░╚═╝░░╚══╝░░'; sleep 1
-echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
+echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░█████╗░███╗░░██╗██╗███╗░░░███╗███████╗░██████╗░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░██╔══██╗████╗░██║██║████╗░████║██╔════╝██╔════╝░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░███████║██╔██╗██║██║██╔████╔██║█████╗░░╚█████╗░░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░██╔══██║██║╚████║██║██║╚██╔╝██║██╔══╝░░░╚═══██╗░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░██║░░██║██║░╚███║██║██║░╚═╝░██║███████╗██████╔╝░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░░░░╚═╝╚══════╝╚═════╝░░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░██╗░░██╗░█████╗░██████╗░██╗███████╗░█████╗░███╗░░██╗░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░██║░░██║██╔══██╗██╔══██╗██║╚════██║██╔══██╗████╗░██║░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░███████║██║░░██║██████╔╝██║░░███╔═╝██║░░██║██╔██╗██║░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░██╔══██║██║░░██║██╔══██╗██║██╔══╝░░██║░░██║██║╚████║░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░██║░░██║╚█████╔╝██║░░██║██║███████╗╚█████╔╝██║░╚███║░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚══════╝░╚════╝░╚═╝░░╚══╝░░░░░░░░░░'; sleep 1
+echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
 
 login() {
-	USER=$(zenity --forms --title="$TITLE" --add-entry="Email/Usuário" --add-password="Senha");
-	echo $USER
+	init() {
+		DADOS=$(zenity --forms --title="$TITLE" --add-entry="Email/Usuário" --add-password="Senha");
+
+		EMAIL=$(echo "$DADOS" | cut -d, -f1)
+		PASS=$(echo "$DADOS" | cut -d, -f2)		
+	}
+
+	init
+
 }
 
 register() {
-	USER=$(zenity --forms --title="$TITLE" --add-entry="Nome de Usuário" --add-entry="Email" --add-password="Senha" --add-password="Digita a senha novamente.")
-	echo $USER
+	checker_data() {
+		if [[ " " -ne $USER ]]; then
+			if [[ " " -ne $EMAIL ]]; then
+				 EMAIL_CHECKER=$(echo "$EMAIL" | grep "@gmail.com")
+				 if [[  " " -ne $EMAIL_CHECKER ]]; then
+				 	if [[ " " -ne $PASS ]]; then
+						case $PASS_RETRY in
+							$PASS)
+								zenity --notification --text="Ola $USER, seja bem vindo ao Animes Horizon!"
+								echo -e "$DADOS\n$(cat ./databases/users.txt)" > ./databases/users.txt
+								sleep 1
+								./views/index.sh
+							;;
+							**)
+								zenity --error --title="$TITLE" --text="As senhas não se coincidem !" --no-wrap --ellipsize
+								init
+							;;
+						esac
+					else
+						zenity --error --title="$TITLE" --text="Você precisa adicionar uma senha." --no-wrap --ellipsize
+						init
+				 	fi
+				else
+					zenity --error --title="$TITLE" --text="Este email não é valido." --no-wrap --ellipsize
+					init
+				fi
+			else
+				zenity --error --title="$TITLE" --text="Você precisa adicionar um email." --no-wrap --ellipsize
+				init
+			fi
+		else
+			zenity --error --title="$TITLE" --text="Você precisa colocar um nome de usuário." --no-wrap --ellipsize
+			init
+		fi
+	}
+
+	init() {
+		DADOS=$(zenity --forms --title="$TITLE" --add-entry="Nome de Usuário" --add-entry="Email" --add-password="Senha" --add-password="Digita a senha novamente." --separator=",")
+
+		USER=$(echo "$DADOS" | cut -d, -f1)
+		EMAIL=$(echo "$DADOS" | cut -d, -f2)
+		PASS=$(echo "$DADOS" | cut -d, -f3)
+		PASS_RETRY=$(echo "$DADOS" | cut -d, -f4)
+		
+		checker_data
+	}
+
+	init
 }
 
 home() {
---
+++ views/index.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'All Done'
+
+zenity --text-info --filename="../README.md"
